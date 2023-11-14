feac4a02ebd0ad0374a78bd4ddf1b9ca6ffdd9b5
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2023-11-14 09:55:09 +0100

    Fixed PaymentMethod
    Let the user choose his payment method

--- src/main/java/T2/N2/ShoesStore.java
+++ src/main/java/T2/N2/ShoesStore.java
@@ -1,21 +1,56 @@
 package T2.N2;
 
 import T2.N2.PaymentMethod.BankAccount;
+import T2.N2.PaymentMethod.CreditCard;
+import T2.N2.PaymentMethod.Paypal;
+
+import java.util.Scanner;
 
 public class ShoesStore implements PaymentCallBack {
+    static Scanner sc = new Scanner(System.in);
+
     public void makePayment() {
         PaymentGateway pG = new PaymentGateway();
-
-        // Simulate different payment methods *****
 
         /*CreditCard creditCard = new CreditCard("1234-5678-9012-3456", "Juan Moreno", 1000);
         pG.makePayment(creditCard, this);
         Paypal paypal = new Paypal("email@email.com", 100);
-        pG.makePayment(paypal, this);*/
+        pG.makePayment(paypal, this);
         BankAccount bankAccount = new BankAccount("1234-5678-9012-3456", 500);
-        pG.makePayment(bankAccount, this);
-    }
+        pG.makePayment(bankAccount, this);*/
 
+        System.out.println("Choose your payment method:");
+        System.out.println("1. Credit Card\n2. Paypal\n3. Bank Account");
+        Byte option = sc.nextByte();
+        switch (option) {
+            case 1 -> {
+                System.out.println("Enter your credit card number:");
+                String creditCardNumber = sc.next();
+                System.out.println("Enter your name:");
+                String name = sc.next();
+                System.out.println("Enter your credit card balance:");
+                Double balance = sc.nextDouble();
+                CreditCard creditCard = new CreditCard(creditCardNumber, name, balance);
+                pG.makePayment(creditCard, this);
+            }
+            case 2 -> {
+                System.out.println("Enter your email:");
+                String email = sc.next();
+                System.out.println("Enter your paypal balance:");
+                Double balance = sc.nextDouble();
+                Paypal paypal = new Paypal(email, balance);
+                pG.makePayment(paypal, this);
+            }
+            case 3 -> {
+                System.out.println("Enter your bank account number:");
+                String bankAccountNumber = sc.next();
+                System.out.println("Enter your bank account balance:");
+                Double balance = sc.nextDouble();
+                BankAccount bankAccount = new BankAccount(bankAccountNumber, balance);
+                pG.makePayment(bankAccount, this);
+            }
+        }
+    }
     @Override
     public void paymentSuccess() {
         System.out.println("Payment successful. Thanks for buying from Shoes Store!");
